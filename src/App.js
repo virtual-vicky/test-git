6449b60966aa6908a17add9246e0e8b1684ecd7d
Hitend D <Vicky.speeedy@gmail.com>
2022-10-07 10:42:41 +0530

    loginF-skeloton

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/auth" element={<Auth />} />
+        <Route path="/" element={<Auth />} />
       </Routes>
     </BrowserRouter>
   )
